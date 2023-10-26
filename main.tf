1c2dc509d8b2131f814fb7895f8330346b240967
Mitch Duncan <mitch@mitchduncan.com>
2023-10-26 10:01:59 -0700

    added cloud block

--- main.tf
+++ main.tf
@@ -1,3 +1,25 @@
+terraform {
+  cloud {
+    organization = "mitchduncan"
+    workspaces {
+      name = "ec2-instance"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_kms_key" "a" {
   description             = "KMS key 1"
   deletion_window_in_days = 10
