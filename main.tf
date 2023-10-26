58eee688fa3085e101162f8988584c4f7c6b8254
Mitch Duncan <mitch@mitchduncan.com>
2023-10-26 10:52:20 -0700

    added IDR tagging

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   cloud {
     organization = "mitchduncan"
     workspaces {
-      name = "ec2-instance"
+      name = "aws-kms-keys"
     }
   }
 
@@ -20,13 +20,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_kms_key" "a" {
+resource "aws_kms_key" "IDR_kms_key" {
   description             = "KMS key 1"
   deletion_window_in_days = 10
 }
 
-resource "aws_sqs_queue" "terraform_queue" {
-  name                              = "terraform-example-queue"
+resource "aws_sqs_queue" "idr_sqs_queue" {
+  name                              = "IDR-sqs-queue"
   kms_master_key_id                 = "alias/aws/sqs"
   kms_data_key_reuse_period_seconds = 300
 }
--- policy.tf
+++ policy.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_policy" "policy" {
-  name        = "test_policy"
+  name        = "idr_policy"
   path        = "/"
-  description = "My test policy"
+  description = "IDRInsight test policy"
 
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
@@ -10,12 +10,11 @@ resource "aws_iam_policy" "policy" {
     Statement = [
       {
         Action = [
-          "ec2:Describe*",
           "sqs:ReceiveMessage",
           "sqs:DeleteMessage"
         ]
         Effect   = "Allow"
-        Resource = "arn:aws:sqs:*:674565636223:terraform_queue"
+        Resource = "arn:aws:sqs:*:674565636223:idr_sqs_queue"
       },
     ]
   })
